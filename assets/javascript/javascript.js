0c03f6f28be2d779f0f72ab29cde77e88accf709
Stepan <stepos36@gmail.com>
2018-11-29 01:57:43 -0500

    static crystals fully func // losses and wins counter

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,7 @@
 let total = 0
+let random = getRandomInteger(19, 120)
+let wins = 0
+let losses = 0
 
 $( document ).ready(function() {
 $('#score').html(total)
@@ -6,46 +9,52 @@ $("#rules").hide()
 $('#infoshow').on('click', function() {
     $("#rules").slideToggle()
 })
-$('#aim').html(getRandomInteger(19, 120))
-$('#aim').val(getRandomInteger(19, 120))
+$('#aim').html(random)
 
 var diamond1= getRandomInteger(1, 12)
 var diamond2= getRandomInteger(1, 12)
 var diamond3= getRandomInteger(1, 12)
 var diamond4= getRandomInteger(1, 12)
 
-
-
 $('#diamond1').on('click', function() {
     total = total + diamond1
     $('#score').html(total)
-    $('#score').val(total)
+    checkResult()
 })
 $('#diamond2').on('click', function() {
     total = total + diamond2
     $('#score').html(total)
-    $('#score').val(total)
+    checkResult()
 })
 $('#diamond3').on('click', function() {
     total = total + diamond3
     $('#score').html(total)
-    $('#score').val(total)
+    checkResult()
 })
 $('#diamond4').on('click', function() {
     total = total + diamond4
     $('#score').html(total)
-    $('#score').val(total)
+    checkResult()
 })
-
-if ($('#aim').val() === $('#score').val()) {
-    alert('You win!')
-}
-if ($('#aim').val() < $('#score').val()) {
-    alert('You lose!')
-}
 })
 function getRandomInteger(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function checkResult() {
+    if (total === random) {
+        alert('You win!')
+        wins++
+        $('#wins').html(wins)
+        total = 0
+        $('#score').html(total)
+    }
+    if (total > random) {
+        alert('You lose!')
+        losses++
+        $('#losses').html(losses)
+        total = 0
+        $('#score').html(total)
+    }
 }
